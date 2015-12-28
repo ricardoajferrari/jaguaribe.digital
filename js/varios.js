922f12475c7bf58fa8ce00b88f00b1f2019b8aa0
jagua <ricardoajferrari@gmail.com>
2015-12-28 08:40:46 -0200

    Menuzin funcionando plenamente. Link PDF idem. Testes Firefox, Chrome e Safari - iPad. No navegador nativo do Android ainda deslocamento pra direita do fundo topo.

--- js/varios.js
+++ js/varios.js
@@ -1,49 +1,31 @@
 // JavaScript Document
 
 $(document).ready(function() {
+	
 	$("#botao_english").click(function() {
 	    $("#logo_menu_idiomas").hide();
 	    $('head').append('<link rel="stylesheet" href="css/blur.css" type="text/css" />');
 		$('head').append('<link rel="stylesheet" href="css/english.css" type="text/css" />');
 		$.getScript( "js/bg_fundo_top_english.js");
-
 	});
+
 	$("#botao_portugues").click(function() {
 	    $("#logo_menu_idiomas").hide();
 	    $('head').append('<link rel="stylesheet" href="css/blur.css" type="text/css" />');
 		$('head').append('<link rel="stylesheet" href="css/portugues.css" type="text/css" />');
 		$.getScript( "js/bg_fundo_top_portugues.js");
 	});
-	$(".acesso_menuzin").click(function() {
-		$('head').append('<link rel="stylesheet" href="css/menuzin_english.css" type="text/css" />');
-	});
 
-	$('#resume').click(function(){
+    $(".acesso_menuzin").click(function(){
+        $("#texto").toggle(1, function(){
+        });
+        $("#box_menuzin").toggle(1, function(){
+        });
+    });
+
+	$('.resume').click(function(){
 	 var popup = window.open('others/ricardo.ferrari_resume.pdf', '_blank');
 		var dom = popup.document.body;
-  
-});
+	});
 
 });
-
- /*
-	$( "#botao_english" ).on( "load", function() {
-	$( this ).css( "pointer-events", "auto" );
-});*/
-
-/*
-var viudeo = document.getElementById("video1");
-function toca() { 
-	viudeo.play(); 
-}
-*/
-
-
-/*
-function toggle(obj) {
-	$( "#botao_english" ).css( "pointer-events", "none" );	
-	if ( botao_espanol.style.opacity = '100' ) {
-		botao_portuguese.style."pointer-events" = 'visible';
-	}
-}
-*/
