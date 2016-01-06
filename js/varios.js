7db34a198e0ae160eed4db6dcf19102711fd3780
jagua <ricardoajferrari@gmail.com>
2016-01-06 11:36:24 -0200

    Menuzin em ingles perfeito Mas partir pra tabela, pq flexbox bug safari Novas imagens botoes mobile first

--- js/varios.js
+++ js/varios.js
@@ -15,20 +15,22 @@ $(document).ready(function() {
 		$('head').append('<link rel="stylesheet" href="css/portugues.css" type="text/css" />');
 		$.getScript( "js/bg_fundo_top_portugues.js");
 	});
-
+	
     $(".acesso_menuzin").click(function(){
         $("#texto").toggle(1, function(){
         });
         $("#box_menuzin").toggle(1, function(){
         });
     });
-
+	
 	$('.resume').click(function(){
-	 var popup = window.open('others/ricardo.ferrari_resume.pdf', '_blank');
+		var popup = window.open('others/ricardo.ferrari_resume.pdf', '_blank');
 		var dom = popup.document.body;
 	});
-	
-	//para o safari:
-	$(document).scrollTop(0);
-
 });
+
+/*
+
+	
+
+*/
