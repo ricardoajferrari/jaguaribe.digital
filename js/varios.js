d6f17d24747d529ffed47567fd5efba9225756b9
jagua <ricardoajferrari@gmail.com>
2016-01-15 12:45:18 -0200

    Em ingles, para medias e baixas resolucoes, acesso form OK

--- js/varios.js
+++ js/varios.js
@@ -23,6 +23,27 @@ $(document).ready(function() {
         });
     });
 	
+	$(".acesso_menuzin_form").click(function() {
+		$(".box_menuzin").css("display", "block");	
+		$(".form").css("display", "none");
+		$(".acesso_menuzin").css("display", "inline-block");	
+		$(".acesso_menuzin_form").css("display", "none");	
+	});
+	/*
+    $(".fire_form").click(function(){
+        $(".texto").toggle(1, function(){
+        });
+        $(".form").toggle(1, function(){
+        });
+    });
+	
+	$(".fire_form_pq").click(function(){
+		$(".box_menuzin").toggle(1, function(){
+		});
+		$(".form").toggle(1, function(){
+		});
+	});
+	*/
 	$('.resume').click(function(){
 		var popup = window.open('others/ricardo.ferrari_resume.pdf', '_blank');
 		var dom = popup.document.body;
@@ -31,14 +52,40 @@ $(document).ready(function() {
 		var popup = window.open('others/ricardo.ferrari_curriculo.pdf', '_blank');
 		var dom = popup.document.body;
 	});
-		
-	//para o safari:
-    $(document).scrollTop(0);
 	
 });
 
+//para o Safari no iPhone:
+$(window).load(function() {
+	function isiPhone(){
+		return (
+			(navigator.platform.indexOf("iPhone") != -1)     );
+		}
+		if(isiPhone()){
+			$(".container_english").css("top", "1em");
+			$(".box_menuzin").css("top", "2.5em");					
+	}
+		
+	$(".fire_form_pq").click(function() {
+		$(".acesso_menuzin").css("display", "none");	
+		$(".acesso_menuzin_form").css("display", "inline-block");	
+		$(".form").css("display", "block");	
+		$(".box_menuzin").css("display", "none");	
+	});
+		
+
+});
+
 /*
-
+if ($('.form').css('display') == 'block')	{
+		    alert("The paragraph was clicked.");
+}
+*/	
 	
+		/*
+		else	{
+			$(".container_english").css("top", "0em");
+		}
 
-*/
+/*
+		*/
