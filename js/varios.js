30f34e57a1de279e5e9b74f737a3d83c7afbef32
jagua <ricardoajferrari@gmail.com>
2016-01-16 18:42:12 -0200

    Acesso form em ingles OK inclusive com resizing

--- js/varios.js
+++ js/varios.js
@@ -21,22 +21,20 @@ $(document).ready(function() {
         });
         $(".box_menuzin").toggle(1, function(){
         });
-    });
-	
+    });	
 	$(".acesso_menuzin_form").click(function() {
 		$(".box_menuzin").css("display", "block");	
 		$(".form").css("display", "none");
 		$(".acesso_menuzin").css("display", "inline-block");	
 		$(".acesso_menuzin_form").css("display", "none");	
 	});
+	$(".acesso_menuzin_form_2").click(function() {
+		$(".box_menuzin").css("display", "block");	
+		$(".form").css("display", "none");
+		$(".acesso_menuzin").css("display", "inline-block");	
+		$(".acesso_menuzin_form_2").css("display", "none");	
+	});
 	/*
-    $(".fire_form").click(function(){
-        $(".texto").toggle(1, function(){
-        });
-        $(".form").toggle(1, function(){
-        });
-    });
-	
 	$(".fire_form_pq").click(function(){
 		$(".box_menuzin").toggle(1, function(){
 		});
@@ -44,6 +42,7 @@ $(document).ready(function() {
 		});
 	});
 	*/
+
 	$('.resume').click(function(){
 		var popup = window.open('others/ricardo.ferrari_resume.pdf', '_blank');
 		var dom = popup.document.body;
@@ -55,16 +54,18 @@ $(document).ready(function() {
 	
 });
 
-//para o Safari no iPhone:
 $(window).load(function() {
+//para o Safari no iPhone:
 	function isiPhone(){
 		return (
 			(navigator.platform.indexOf("iPhone") != -1)     );
 		}
 		if(isiPhone()){
 			$(".container_english").css("top", "1em");
+			$(".container_portugues").css("top", "1em");
 			$(".box_menuzin").css("top", "2.5em");					
 	}
+//
 		
 	$(".fire_form_pq").click(function() {
 		$(".acesso_menuzin").css("display", "none");	
@@ -72,12 +73,27 @@ $(window).load(function() {
 		$(".form").css("display", "block");	
 		$(".box_menuzin").css("display", "none");	
 	});
-		
+    $(".fire_form").click(function(){
+		$(".acesso_menuzin").css("display", "none");	
+		$(".acesso_menuzin_form_2").css("display", "inline-block");	
+		$(".form").css("display", "block");	
+		$(".box_menuzin").css("display", "none");		
+		$(".texto").css("display", "none");
+		$(".texto_2").css("display", "none");		
+    });		
+});
 
+$( window ).resize(function() {
+	if	(($('.form').css('display') == 'block') || ($('.box_menuzin').css('display') == 'block') &&	($(window).width() <= 600))	{
+			$('.texto').css('display', 'none');
+	}
+	else	{
+			$(".texto").css("display", "block");				
+	}
 });
 
 /*
-if ($('.form').css('display') == 'block')	{
+if ($('.form').css('display') !== 'none')	{
 		    alert("The paragraph was clicked.");
 }
 */	
